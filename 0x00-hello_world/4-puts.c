31d5d5e498a24239abd3b5a981346a8a9aa6c392
abumuizzah <ahallomah@gmail.com>
2022-10-13 22:39:22 -0700

    puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Printing
+ * 
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
