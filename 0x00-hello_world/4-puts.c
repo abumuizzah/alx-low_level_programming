e98476b08284e3fd77378a1339946d3594fb9025
abumuizzah <ahallomah@gmail.com>
2022-10-13 23:34:41 -0700

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,9 @@
  *main-Printing
  * 
  *Return: Always 0 (Successful)
- * /
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"); return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
